823f463637fc9ed624c53f2f76498c5387eee98e
Ruslan <ruslanbk11@mail.ru>
2018-10-11 19:30:16 +0300

    Eslintered

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,8 +1,8 @@
 const say = function say() {
-  let d = document.createElement('div');
-  let content = document.createTextNode('Hello World');
-  d.className = "block";
-  d.appendChild(content); 
+  const d = document.createElement('div');
+  const content = document.createTextNode('Hello World');
+  d.className = 'block';
+  d.appendChild(content);
   document.body.appendChild(d);
 };
 
