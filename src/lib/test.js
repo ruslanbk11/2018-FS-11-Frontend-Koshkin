1140b4d11236268b61dbfb859df2dd4ef426f930
Ruslan <ruslanbk11@mail.ru>
2018-10-11 18:56:37 +0300

    added css styles

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,10 +1,8 @@
 const say = function say() {
-  const d = document.createElement('div');
-  const content = document.createTextNode('Hello World');
-  d.appendChild(content);
-  d.style.width = '200px';
-  d.style.height = '100px';
-  d.style.background = 'red';
+  let d = document.createElement('div');
+  let content = document.createTextNode('Hello World');
+  d.className = "block";
+  d.appendChild(content); 
   document.body.appendChild(d);
 };
 
